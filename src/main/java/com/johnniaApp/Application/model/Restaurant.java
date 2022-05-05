dafca03a7f15b155e785e4a916d15b1f2225c661
johnnia001 <jarroyo.staff@gmail.com>
2022-05-04 21:49:53 -0600

    make restaurant model

--- src/main/java/com/johnniaApp/Application/model/Restaurant.java
+++ src/main/java/com/johnniaApp/Application/model/Restaurant.java
@@ -1,4 +1,55 @@
 package com.johnniaApp.Application.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Restaurant {
+    // primary key
+    @Id
+    //auto inclement id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String Name;
+    private String Address;
+    private String FoodItems;
+
+    // constructor
+    public Restaurant() {
+    }
+
+
+    // getter setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getFoodItems() {
+        return FoodItems;
+    }
+
+    public void setFoodItems(String foodItems) {
+        FoodItems = foodItems;
+    }
 }
