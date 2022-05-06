38ae68041095a29e25071334589d17ea9e82ee60
johnnia001 <jarroyo.staff@gmail.com>
2022-05-05 20:10:46 -0600

    make repository, connect to model

--- src/main/java/com/johnniaApp/Application/repository/RestaurantRepository.java
+++ src/main/java/com/johnniaApp/Application/repository/RestaurantRepository.java
@@ -1,4 +1,9 @@
 package com.johnniaApp.Application.repository;
+// import class
+import com.johnniaApp.Application.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RestaurantRepository {
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
 }
